9881ec16f580ed244559dac43373aed82c626462
mychanal <mychanal@ust.hk>
2018-04-26 15:44:48 +0800

    //shit

--- app/src/main/java/com/example/eddy/quicklaunchapp/MainActivity.java
+++ app/src/main/java/com/example/eddy/quicklaunchapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    //shit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
